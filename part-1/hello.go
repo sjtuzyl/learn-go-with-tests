04dbf7595e08b1a23cfba49544038728848d500f
sjtuzyl <z1377527269@163.com>
2022-10-26 20:02:55 +0800

    [upd]switch

--- part-1/hello.go
+++ part-1/hello.go
@@ -3,15 +3,31 @@ package main
 import "fmt"
 
 // 避免每次使用Hello时创建字符串实例
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }

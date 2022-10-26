6c97e14c6269b31389af6bcdbdbcec7ee30de200
sjtuzyl <z1377527269@163.com>
2022-10-26 19:45:39 +0800

    [upd]子测试

--- part-1/hello_test.go
+++ part-1/hello_test.go
@@ -10,12 +10,25 @@ import (
 // 2. 测试函数的命名必须以 Test 开始
 // 3. 测试函数只接受一个参数 t *testing.T
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
 
-	if got != want {
-		t.Errorf("got '%q' want '%q'", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
 
 func BenchmarkRandInt(b *testing.B) {

7fc10987fe1b3741a6ab97c11b030a5dfea25204
sjtuzyl <z1377527269@163.com>
2022-11-16 15:54:30 +0800

    [upd]reflect-2

--- part-12/ref.go
+++ part-12/ref.go
@@ -12,21 +12,94 @@ import "reflect"
 
 func Walk(x interface{}, fn func(input string)) {
 	// fn("KRDOG")
-	val := reflect.ValueOf(x)
+	val := getValue(x)
 
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
+	// 1.虽然可行，但是恶心
+	// if val.Kind() == reflect.Slice {
+	// 	for i := 0; i < val.Len(); i++ {
+	// 		Walk(val.Index(i).Interface(), fn)
+	// 	}
+	// 	return
+	// }
 
-		// if field.Kind() == reflect.String {
-		// 	fn(field.String())
-		// } else if field.Kind() == reflect.Struct {
-		// 	Walk(field.Interface(), fn)
-		// }
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			Walk(field.Interface(), fn)
-		}
+	// for i := 0; i < val.NumField(); i++ {
+	// 	field := val.Field(i)
+
+	// 	// if field.Kind() == reflect.String {
+	// 	// 	fn(field.String())
+	// 	// } else if field.Kind() == reflect.Struct {
+	// 	// 	Walk(field.Interface(), fn)
+	// 	// }
+	// 	switch field.Kind() {
+	// 	case reflect.String:
+	// 		fn(field.String())
+	// 	case reflect.Struct:
+	// 		Walk(field.Interface(), fn)
+	// 	}
+	// }
+
+	// 2.还可以做得更好
+	// switch val.Kind() {
+	// case reflect.Struct:
+	// 	for i := 0; i < val.NumField(); i++ {
+	// 		Walk(val.Field(i).Interface(), fn)
+	// 	}
+	// case reflect.Slice:
+	// 	for i := 0; i < val.Len(); i++ {
+	// 		Walk(val.Index(i).Interface(), fn) //
+	// 	}
+	// case reflect.String:
+	// 	fn(val.String())
+	// }
+
+	// 3.再次重构
+	// numOfValues := 0
+	// var getField func(int) reflect.Value
+	// switch val.Kind() {
+	// case reflect.Struct:
+	// 	numOfValues = val.NumField()
+	// 	getField = val.Field
+	// case reflect.Slice, reflect.Array:
+	// 	numOfValues = val.Len()
+	// 	getField = val.Index
+	// case reflect.String:
+	// 	fn(val.String())
+	// case reflect.Map:
+	// 	for _, key := range val.MapKeys() {
+	// 		Walk(val.MapIndex(key).Interface(), fn)
+	// 	}
+	// }
+
+	// for i := 0; i < numOfValues; i++ {
+	// 	Walk(getField(i).Interface(), fn)
+	// }
+
+	waleValue := func(value reflect.Value) {
+		Walk(value.Interface(), fn)
 	}
+	switch val.Kind() {
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			waleValue(val.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			waleValue(val.Index(i))
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			waleValue(val.MapIndex(key))
+		}
+	case reflect.String:
+		fn(val.String())
+	}
+
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+	return val
 }
--- part-12/ref_test.go
+++ part-12/ref_test.go
@@ -70,6 +70,27 @@ func TestWalk2(t *testing.T) {
 			Person{"Chirs", Profile{33, "London"}},
 			[]string{"Chirs", "London"},
 		},
+		{
+			"Pointers to things",
+			&Person{"Chirs", Profile{33, "London"}},
+			[]string{"Chirs", "London"},
+		},
+		{
+			"Slices",
+			[]Profile{
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
+		},
+		{
+			"Arrays",
+			[2]Profile{
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
+		},
 	}
 
 	for _, test := range cases {
@@ -85,4 +106,31 @@ func TestWalk2(t *testing.T) {
 		})
 	}
 
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		Walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+}
+
+func assertContains(t *testing.T, haystack []string, needle string) {
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+
+	if !contains {
+		t.Errorf("expected %+v to contain '%s' but it didn't", haystack, needle)
+	}
 }
